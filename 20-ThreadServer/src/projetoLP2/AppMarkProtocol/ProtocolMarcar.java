d0b6bab4978f03cc390f935644a8fec6019ed126
Marcus Vinícius Maracajá Pires <50149771+MarcusMaracaja@users.noreply.github.com>
2022-06-23 19:21:26 -0300

    Add files via upload

---
+++ 20-ThreadServer/src/projetoLP2/AppMarkProtocol/ProtocolMarcar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoLP2.AppMarkProtocol;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Marcus
+ */
+public class ProtocolMarcar implements Serializable{
+    
+    
+}
